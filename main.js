c931ff3ebd05d4f77f225167423f9944aaebed56
avearmin <117543612+avearmin@users.noreply.github.com>
2023-04-21 22:45:22 -0400

    Create logic for setting the operator on button click
    This commit creates the function for recording the operator the user clicks on. It is defined within the 'arithmeticOperation' object to more easily manipulate the 'operator' property within the same object. 
    
    In addition, the function calls for 'addNumbersToDisplay' and 'addOperatorToDisplay' were removed, and the definitions commented out, so they can be refactored in the future solely using JavaScript data structures.

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ function main() {
     let arithmeticOperation = {
         operator:null,
         operand1:null,
-        operand2:null
+        operand2:null,
+        setOperatorOnClick: function(elementIds) {
+	    operators = Object.values(elementIds.buttons.operators).filter(item => item.id !== elementIds.buttons.operators.equals.id);
+            operators.forEach(operator => {
+                operator.id.addEventListener("click", () => {
+		    this.operator = operator.value;
+        	});
+    	    });
+	}
     };
 
     const elementIds = {
@@ -17,7 +25,7 @@ function main() {
                 five: {id: document.getElementById("five"), value: "5"},
                 six: {id: document.getElementById("six"), value: "6"},
                 seven: {id: document.getElementById("seven"), value: "7"},
-                eight: {id: document.getElementById("eight"), value "8"},
+                eight: {id: document.getElementById("eight"), value: "8"},
                 nine: {id: document.getElementById("nine"), value: "9"},
                 decimal: {id: document.getElementById("decimal"), value: "."}
             },
@@ -35,27 +43,26 @@ function main() {
             }
         }
     };
-    addNumbersToDisplay(elementIds);
-    addOperatorToDisplay(elementIds);
+    arithmeticOperation.setOperatorOnClick(elementIds);
 }
 
-function addNumbersToDisplay(elementIds) {
-    numberIds = Object.values(elementIds.buttons.numbers);
-    numberIds.forEach(numberId => {
-        numberId.addEventListener("click", () => {
-            elementIds.display.textContent += numberId.textContent;
-        });
-    });
-}
+//function addNumbersToDisplay(elementIds) {
+//    numberIds = Object.values(elementIds.buttons.numbers);
+//    numberIds.forEach(numberId => {
+//        numberId.addEventListener("click", () => {
+//            elementIds.display.textContent += numberId.textContent;
+//        });
+//    });
+//}
 
-function addOperatorToDisplay(elementIds) {
-    operatorIds = Object.values(elementIds.buttons.operators).filter(item => item !== elementIds.buttons.operators.equals);
-    operatorIds.forEach(operatorId => {
-        operatorId.addEventListener("click", () => {
-            elementIds.display.textContent += operatorId.textContent;
-        });
-    });
-}
+//function addOperatorToDisplay(elementIds) {
+//    operatorIds = Object.values(elementIds.buttons.operators).filter(item => item !== elementIds.buttons.operators.equals);
+//    operatorIds.forEach(operatorId => {
+//        operatorId.addEventListener("click", () => {
+//            elementIds.display.textContent += operatorId.textContent;
+//        });
+//    });
+//}
 
 function operate(operator, operand1, operand2) {
     if (operator === '+') {
